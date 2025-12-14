f205fd2a6d25fd6b54136f3e57e17d420e0a7f3b
fuadhasan199 <fuadhasan740266@gmail.com>
2025-12-14 20:09:01 +0600

    express mongodb setup and available loan api

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express') 
+require('dotenv').config() 
+const cors=require('cors') 
+
+const app = express()
+const port = 3000 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_password}@cluster0.e1kqjp5.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.use(express.json())
+app.use(cors()) 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})  
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect(); 
+
+    const db=client.db('loan-link') 
+    const availableLoan=db.collection('availableLoan') 
+
+
+
+
+    app.post('/availableloan',async(req,res)=>{
+        const loan=req.body 
+        const result=await availableLoan.insertOne(loan)
+        res.send(result)
+    }) 
+
+    app.get('/availableloan',async(req,res)=>{
+        const result=await availableLoan.find().toArray()
+        res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
